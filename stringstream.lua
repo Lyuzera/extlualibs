5aff45e3d68a5dcb0945d0d534f1c58ba72e2a6f
Lyuzera <imlyuzera@gmail.com>
2020-07-15 16:25:43 -0300

    add stringstream.lua

---
+++ stringstream.lua
@@ -0,0 +1,63 @@
+--[[
+	Copyright (c) 2020 Moisés dos Santos
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+]]
+
+local stringstream = {
+	_VERSION = 'stringstream.lua v2020.15.07',
+	_URL = 'https://github.com/Lyuzera/extlualibs/stringstream.lua'
+}
+
+setmetatable(stringstream, {
+	__call = function(self)
+		local obj = {}
+		return setmetatable(obj, {__index = self})
+	end
+})
+
+-- Insert formatted string
+function stringstream:append(str, ...)
+	self[#self + 1] = str:format(...)
+end
+
+-- Get content
+function stringstream:str()
+	return table.concat(self)
+end
+
+-- clears the contents
+function stringstream:clear()
+	for index in ipairs(self) do
+		self[index] = nil
+	end
+end
+stringstream.reset = stringstream.clear
+
+-- print the content
+function stringstream:printf(clear)
+	print(self:str())
+	
+	clear = clear or true
+	if clear then
+		self:clear()
+	end
+end
+
+return stringstream
