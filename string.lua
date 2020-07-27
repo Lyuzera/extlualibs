a34d3b02e9576d53e3fd994903cb818576ac3f9e
Lyuzera <imlyuzera@gmail.com>
2020-07-27 15:20:47 -0300

    added string.lua

---
+++ string.lua
@@ -0,0 +1,49 @@
+--[[
+	Copyright (c) 2020 Moisés dos Santos
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+]]
+
+string.plural = function(str, number)
+	return number > 1 and str .. 's' or str
+end
+
+string.empty = function(self)
+	return self == ''
+end
+
+string.split = function(str, sep)
+	local res = {}
+	for v in str:gmatch("([^" .. sep .. "]+)") do
+		res[#res + 1] = v:trim()
+	end
+	return res
+end
+
+string.trim = function(str)
+	return str:match'^()%s*$' and '' or str:match'^%s*(.*%S)'
+end
+
+string.starts = function(str, substr)
+	return string.sub(str, 1, #substr) == substr
+end
+
+string.titleCase = function(str)
+	return str:gsub("(%a)([%w_']*)", function(first, rest) return first:upper() .. rest:lower() end)
+end
